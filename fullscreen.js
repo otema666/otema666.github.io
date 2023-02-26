dfc46eaea0d5f0b394575eb7614bab6cdbce23a1
otema666 <126337147+otema666@users.noreply.github.com>
2023-02-26 18:48:34 +0100

    Update fullscreen.js

--- fullscreen.js
+++ fullscreen.js
@@ -1,3 +1,6 @@
+window.onbeforeunload = function() {
+    return true;
+};
 function aceptar() {
 	document.getElementById("contenido").style.display = "block";
 	document.getElementById("mensaje").style.display = "none";
