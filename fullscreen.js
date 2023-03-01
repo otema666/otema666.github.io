4f35458028b33f582a7f43e65324389c828000d4
otema666 <126337147+otema666@users.noreply.github.com>
2023-03-01 23:55:08 +0100

    Add files via upload

--- fullscreen.js
+++ fullscreen.js
@@ -1,6 +1,3 @@
-window.onbeforeunload = function() {
-    return true;
-};
 function aceptar() {
 	document.getElementById("contenido").style.display = "block";
 	document.getElementById("mensaje").style.display = "none";
